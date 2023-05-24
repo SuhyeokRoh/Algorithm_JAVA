e9dab8891ae9c494eaa66f1c4f4b85c49bb5a852
Nosu <122436950+SuhyeokRoh@users.noreply.github.com>
2023-05-24 19:03:16 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] tmp = br.readLine().split(" ");
+		int a = Integer.parseInt(tmp[0]);
+		int b = Integer.parseInt(tmp[1]);
+		
+		int rst = Math.min(a/2, b);
+		System.out.println(rst);
+	}
+}
